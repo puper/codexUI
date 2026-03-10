84af837b36c51e345a68547213993150c03894ec
Igor <igor.levochkin@xrtc.io>
2026-03-11 06:03:56 +0700

    Add welcome console log

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,6 @@ import App from './App.vue'
 import router from './router'
 import './style.css'
 
+console.log('Welcome to codexapp. GitHub: https://github.com/friuns/codexui')
+
 createApp(App).use(router).mount('#app')
